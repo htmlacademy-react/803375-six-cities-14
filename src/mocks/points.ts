8716225dcfba64c5876ca0d27cd1125cf565f214
Artificial_Light <hts7808@gmail.com>
2023-11-01 20:50:42 +0300

    module5-task1 | Инициализировал карту, добавил точки на карту

---
+++ src/mocks/points.ts
@@ -0,0 +1,24 @@
+import {Points} from '../types/points';
+
+export const POINTS: Points = [
+  {
+    title: 'Beautiful & luxurious studio at great location 1',
+    lat: 52.3909553943508,
+    lng: 4.85309666406198
+  },
+  {
+    title: 'Beautiful & luxurious studio at great location 2',
+    lat: 52.3609553943508,
+    lng: 4.85309666406198
+  },
+  {
+    title: 'Beautiful & luxurious studio at great location 3',
+    lat: 52.3909553943508,
+    lng: 4.929309666406198
+  },
+  {
+    title: 'Beautiful & luxurious studio at great location 4',
+    lat: 52.3809553943508,
+    lng: 4.939309666406198
+  },
+];
